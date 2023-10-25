1b06565fc15cc79bbd914a849deb08ff3211dd95
Setepenre <pierre.delaunay@outlook.com>
2023-10-24 20:54:23 -0400

    Remove pending test

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,70 +1,70 @@
-from argklass.argformat import DumpParserAction, HelpAction, HelpActionException
-from argklass.arguments import add_arguments, group, subparsers
+# from argklass.argformat import DumpParserAction, HelpAction, HelpActionException
+# from argklass.arguments import add_arguments, group, subparsers
 
 
-@dataclass
-class Undo:
-    """Undo doc"""
+# @dataclass
+# class Undo:
+#     """Undo doc"""
 
-    argument: bool  # arg doc
-
-
-@dataclass
-class Do:
-    """Do doc"""
-
-    argument: bool  # arg doc
+#     argument: bool  # arg doc
 
 
-@dataclass
-class Redo:
-    """Redo doc"""
+# @dataclass
+# class Do:
+#     """Do doc"""
 
-    argument: bool  # arg doc
-
-
-@dataclass()
-class Edit:
-    """Edit doc"""
-
-    editcmd: str = subparser()
-    undo: Undo = parser(Undo)
-    do: Do = parser(Do)
-    redo: Redo = parser(Redo)
+#     argument: bool  # arg doc
 
 
-@dataclass
-class Group:
-    """
-    Group help
-    """
+# @dataclass
+# class Redo:
+#     """Redo doc"""
 
-    verbose: bool  # verbose doc
+#     argument: bool  # arg doc
 
 
-@dataclass
-class CommandLine:
-    """Command line help"""
+# @dataclass()
+# class Edit:
+#     """Edit doc"""
 
-    g: Group = group(Group)
+#     editcmd: str = subparser()
+#     undo: Undo = parser(Undo)
+#     do: Do = parser(Do)
+#     redo: Redo = parser(Redo)
 
-    cmd: str = subparser()  # ignored doc
-    edit: Edit = parser(Edit)  # ignored
+
+# @dataclass
+# class Group:
+#     """
+#     Group help
+#     """
+
+#     verbose: bool  # verbose doc
 
 
-if __name__ == "__main__":
-    parser = ArgumentParser(add_help=False)
-    parser.add_argument(
-        "-h", "--help", action=HelpAction, help="show this help message and exit"
-    )
-    parser.add_argument(
-        "-d", "--d", action=DumpParserAction, help="show this help message and exit"
-    )
-    add_arguments(parser, CommandLine)
+# @dataclass
+# class CommandLine:
+#     """Command line help"""
 
-    try:
-        args = parser.parse_args()
-        print(args)
-        print(args.cmd)
-    except HelpActionException:
-        pass
+#     g: Group = group(Group)
+
+#     cmd: str = subparser()  # ignored doc
+#     edit: Edit = parser(Edit)  # ignored
+
+
+# if __name__ == "__main__":
+#     parser = ArgumentParser(add_help=False)
+#     parser.add_argument(
+#         "-h", "--help", action=HelpAction, help="show this help message and exit"
+#     )
+#     parser.add_argument(
+#         "-d", "--d", action=DumpParserAction, help="show this help message and exit"
+#     )
+#     add_arguments(parser, CommandLine)
+
+#     try:
+#         args = parser.parse_args()
+#         print(args)
+#         print(args.cmd)
+#     except HelpActionException:
+#         pass
