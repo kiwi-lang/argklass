5ebd7cfd9a206cfee28bb8c19e15a23e3fae406d
Setepenre <pierre.delaunay@outlook.com>
2024-02-04 10:02:32 -0500

    Bump version

--- argklass/__init__.py
+++ argklass/__init__.py
@@ -1,5 +1,5 @@
 __descr__ = "Argparse utility"
-__version__ = "1.4.2"
+__version__ = "1.4.3"
 __license__ = "BSD 3-Clause License"
 __author__ = "Pierre Delaunay"
 __author_email__ = "pierre@delaunay.io"
